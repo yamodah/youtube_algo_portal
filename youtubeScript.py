0b097e5626896447a799237d85e1ca628b05487f
yamodah <yassinedahlek@gmail.com>
2022-02-13 16:09:31 -0500

    added for loop increased time and added a go back

--- youtubeScript.py
+++ youtubeScript.py
@@ -3,7 +3,7 @@ from time import sleep
 
 username = input("username: ")
 password = input("password: ")
-search_for = input("what side of youtube would you like to see ? ")
+search_for = input("what side of youtube would you like to see ? ").replace(" ", "+")
 # open web browser and site
 browser = uc.Chrome(use_subprocess=True)
 browser.get("https://www.youtube.com/")
@@ -21,5 +21,7 @@ sleep(1)
 browser.get(f"https://www.youtube.com/results?search_query={search_for}")
 
 # browser.get(f"https://www.youtube.com/results?search_query={search_for}&sp=CAI%253D") use this to search by most recent
-browser.find_element_by_xpath("/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[1]/div[1]/ytd-thumbnail/a").click()
-sleep(10)
+for i in range(1,10):
+    browser.find_element_by_xpath(f"/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[{i}]/div[1]/ytd-thumbnail/a").click()
+    sleep(30)
+    browser.execute_script("window.history.go(-1)")
